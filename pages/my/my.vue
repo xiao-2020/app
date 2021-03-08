fcf1d56fd99ca5fedccde4c9d0073a1de418dd11
liuxiao <liu.xiao@digitalcnzz.com>
2021-03-08 20:49:25 +0800

    修改我的页面

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,15 @@
 <template>
-	<view>
-		
+	<view class="container">
+		<view class="content">
+			<!-- 用户信息 -->
+			<view class="userinfo">
+				
+			</view>
+			<!-- 详细信息 -->
+			<view class="detail-info">
+				
+			</view>
+		</view>
 	</view>
 </template>
 
